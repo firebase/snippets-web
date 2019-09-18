6a01bf1a4816f568230b7bc709cfa47c1faa8805
markarndt <50713862+markarndt@users.noreply.github.com>
2019-09-18 14:49:40 -0700

    Add link to comment and new WebChannel port comment.

--- firestore/emulator-suite.js
+++ firestore/emulator-suite.js
@@ -1,11 +1,12 @@
 
-
 export async function onDocumentReady(firebaseApp) {
 
   //[START fs_emulator_connect]
-  //firebaseApp previously initialized using firebase.initializeApp()
-  const db = firebaseApp.firestore();
+  // firebaseApp previously initialized using firebase.initializeApp().
+  // See <a href="https://firebase.google.com/docs/web/setup">Add Firebase to your JavaScript project</a>.
+  var db = firebaseApp.firestore();
   if (location.hostname === "localhost") {
+    // Note that the Firebase Web SDK must connect to the WebChannel port
     db.settings({
       host: "localhost:8081",
       ssl: false
