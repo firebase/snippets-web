0f88e0ea97e79bef5f1fb7482fac5d41aea6ad0c
Mark Arndt <markarndt@google.com>
2019-09-18 15:15:38 -0700

    Doctor up flush argument.

--- database/emulator-suite.js
+++ database/emulator-suite.js
@@ -3,21 +3,24 @@
 export async function onDocumentReady() {
 
   //[START rtdb_emulator_connect]
-  let firebaseConfig = {
-    // Point to the RTDB emulator running on localhost.
-    // Here we supply database namespace 'foo'.
-    databaseURL: "http://localhost:9000?ns=foo"
-  }
+  if (location.hostname === "localhost") {
 
-  var myApp = firebase.initializeApp(firebaseConfig);
-  const db = myApp.database();
+    var firebaseConfig = {
+      // Point to the RTDB emulator running on localhost.
+      // In almost all cases the ns (namespace) is your project ID.
+      databaseURL: "http://localhost:9000?ns=YOUR_DATABASE_NAMESPACE"
+    }
+
+    var myApp = firebase.initializeApp(firebaseConfig);
+    var db = myApp.database();
+  } 
   // [END rtdb_emulator_connect]
 }
 
-export async function flushRealtimeDatabase(anRTDBReference) {
+export async function flushRealtimeDatabas(firebase) {
 
   //[START rtdb_emulator_flush]
-  // With a database Reference, write null to the root node to delete the database.
-  anRTDBReference.child("/").set(null);
+  // With a database Reference, write null to clear the database.
+  firebase.database().ref().set(null);
   // [END rtdb_emulator_connect]
 }
