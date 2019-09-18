79981d4623a4e6932db1f197c514ad909ac6f457
markarndt <50713862+markarndt@users.noreply.github.com>
2019-09-18 14:50:55 -0700

    Nope, a link won't work here.

--- firestore/emulator-suite.js
+++ firestore/emulator-suite.js
@@ -3,7 +3,6 @@ export async function onDocumentReady(firebaseApp) {
 
   //[START fs_emulator_connect]
   // firebaseApp previously initialized using firebase.initializeApp().
-  // See <a href="https://firebase.google.com/docs/web/setup">Add Firebase to your JavaScript project</a>.
   var db = firebaseApp.firestore();
   if (location.hostname === "localhost") {
     // Note that the Firebase Web SDK must connect to the WebChannel port
