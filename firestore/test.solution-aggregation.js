e26834e539e1dbdd18a5e8f3d7dc2c8db80236e4
Sam <samstern@google.com>
2020-09-02 12:12:44 +0100

    Solution aggregation

--- firestore/test.solution-aggregation.js
+++ firestore/test.solution-aggregation.js
@@ -1,5 +1,5 @@
 // [START sample_doc]
-var arinellDoc = {
+const arinellDoc = {
   name: 'Arinell Pizza',
   avgRating: 4.65,
   numRatings: 683
@@ -7,28 +7,29 @@ var arinellDoc = {
 // [END sample_doc]
 
 describe("firestore-solution-arrays", () => {
-    var db;
-    before(() => {
-        var config = {
-            apiKey: "AIzaSyArvVh6VSdXicubcvIyuB-GZs8ua0m0DTI",
-            authDomain: "firestorequickstarts.firebaseapp.com",
-            projectId: "firestorequickstarts",
-        };
-        var app = firebase.initializeApp(config, "solution-arrays");
-        db = firebase.firestore(app);
+    let db;
+    before(async () => {
+      const { initializeApp } = require("@firebase/app");
+      const { getFirestore, collection, doc, setDoc } = require("@firebase/firestore");
 
-        return db.collection("restaurants")
-            .doc("arinell-pizza")
-            .set(arinellDoc);
+      const config = {
+          apiKey: "AIzaSyArvVh6VSdXicubcvIyuB-GZs8ua0m0DTI",
+          authDomain: "firestorequickstarts.firebaseapp.com",
+          projectId: "firestorequickstarts",
+      };
+      const app = initializeApp(config, "solution-arrays");
+      db = getFirestore(app);
+
+      await setDoc(doc(collection(db, "restaurants"), "arinell-pizza"), arinellDoc);
     });
 
     describe("solution-arrays", () => {
-        it("should get a collection of ratings", () => {
+        it("should get a collection of ratings", async () => {
           // [START get_collection_ratings]
-          db.collection("restaurants")
-            .doc("arinell-pizza")
-            .collection("ratings")
-            .get()
+          const { collection, doc, getDocs } = require("@firebase/firestore");
+
+          const ratingsRef = collection(doc(collection(db, "restaurants"), "arinell-pizza"), "ratings");
+          const ratingsDocs = await getDocs(ratingsRef);
           // [END get_collection_ratings]
         })
     });
