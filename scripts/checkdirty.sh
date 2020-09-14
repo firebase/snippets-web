ef88e1f31da2e497a774a0e68d0ef31fc735bb10
Sam <samstern@google.com>
2020-09-14 14:04:02 +0100

    Add dirty checks

---
+++ scripts/checkdirty.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [[ $(git diff --stat HEAD) != '' ]]; then
+  git diff --stat HEAD
+  echo
+  echo 'Error: git diff is dirty ... did you forget to run "npm run snippets" after adding snippets?'
+  exit 1
+else
+  echo 'Succes: git diff is clean'
+fi
