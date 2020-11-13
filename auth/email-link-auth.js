b9728addb5cf8f2be3f07294589318019dad35de
Sam <samstern@google.com>
2020-11-13 15:08:49 +0000

    Move auth snippets from quickstart-js

--- auth/email-link-auth.js
+++ auth/email-link-auth.js
@@ -35,9 +35,12 @@ function emailLinkSend(email, actionCodeSettings) {
       // Save the email locally so you don't need to ask the user for it again
       // if they open the link on the same device.
       window.localStorage.setItem('emailForSignIn', email);
+      // ...
     })
     .catch((error) => {
-      // Some error occurred, you can inspect the code: error.code
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
     });
   // [END auth_email_link_send]
 }
---
+++ auth/email.js
@@ -0,0 +1,64 @@
+// These samples are intended for Web so this import would normally be
+// done in HTML however using modules here is more convenient for
+// ensuring sample correctness offline.
+import firebase from "firebase/app";
+import "firebase/auth";
+
+function signInWithEmailPassword() {
+  var email = "test@example.com";
+  var password = "hunter2";
+  // [START auth_signin_password]
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((user) => {
+      // Signed in 
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
+  // [END auth_signin_password]
+}
+
+function signUpWithEmailPasswoerd() {
+  var email = "test@example.com";
+  var password = "hunter2";
+  // [START auth_signup_password]
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((user) => {
+      // Signed in 
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ..
+    });
+  // [END auth_signup_password]
+}
+
+function sendEmailVerification() {
+  // [START auth_send_email_verification]
+  firebase.auth().currentUser.sendEmailVerification()
+    .then(() => {
+      // Email verification sent!
+      // ...
+    });
+  // [END auth_send_email_verification]
+}
+
+function sendPasswordReset() {
+  const email = "sam@example.com";
+  // [START auth_send_password_reset]
+  firebase.auth().sendPasswordResetEmail(email)
+    .then(() => {
+      // Password reset email sent!
+      // ..
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ..
+    });
+  // [END auth_send_password_reset]
+}
