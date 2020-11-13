27dbec7a75d5572302580e162b9878c03c00a66f
Sam <samstern@google.com>
2020-11-13 15:48:42 +0000

    Bring in some move auth snippets

--- auth/google-signin.js
+++ auth/google-signin.js
@@ -24,3 +24,51 @@ function googleBuildAndSignIn(id_token) {
   });
   // [END auth_google_build_signin]
 }
+
+// [START auth_google_callback]
+function onSignIn(googleUser) {
+  console.log('Google Auth Response', googleUser);
+  // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+  var unsubscribe = firebase.auth().onAuthStateChanged((firebaseUser) => {
+    unsubscribe();
+    // Check if we are already signed-in Firebase with the correct user.
+    if (!isUserEqual(googleUser, firebaseUser)) {
+      // Build Firebase credential with the Google ID token.
+      var credential = firebase.auth.GoogleAuthProvider.credential(
+          googleUser.getAuthResponse().id_token);
+  
+      // Sign in with credential from the Google user.
+      // [START auth_google_signin_credential]
+      firebase.auth().signInWithCredential(credential).catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+      // [END auth_google_signin_credential]
+    } else {
+      console.log('User already signed-in Firebase.');
+    }
+  });
+}
+// [END auth_google_callback]
+
+// [START auth_google_checksameuser]
+function isUserEqual(googleUser, firebaseUser) {
+  if (firebaseUser) {
+    var providerData = firebaseUser.providerData;
+    for (var i = 0; i < providerData.length; i++) {
+      if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+          providerData[i].uid === googleUser.getBasicProfile().getId()) {
+        // We don't need to reauth the Firebase connection.
+        return true;
+      }
+    }
+  }
+  return false;
+}
+// [END auth_google_checksameuser]
