4c058b15a0aae2273b0f585912f85f8610d9f060
Sam <samstern@google.com>
2020-12-29 12:05:00 +0000

    Regenerate snippets

--- snippets/auth-next/yahoo-oauth/auth_yahoo_provider_params.js
+++ snippets/auth-next/yahoo-oauth/auth_yahoo_provider_params.js
@@ -3,11 +3,11 @@
 //
 // To make edits to the snippets in this file, please edit the source
 
-// [START auth_yahoo_create_provider_params_modular]
+// [START auth_yahoo_provider_params_modular]
 provider.setCustomParameters({
   // Prompt user to re-authenticate to Yahoo.
   prompt: 'login',
   // Localize to French.
   language: 'fr'
 });  
-// [END auth_yahoo_create_provider_params_modular]
+// [END auth_yahoo_provider_params_modular]
