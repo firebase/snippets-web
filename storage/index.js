a4acdcab462db55a1de2873ab51dcfd45f9baf20
Sam <samstern@google.com>
2021-01-21 14:19:41 +0000

    Storage delete file

--- storage/index.js
+++ storage/index.js
@@ -94,6 +94,22 @@ function refFullExample() {
   // [END storage_ref_full_example]
 }
 
+function deleteFile() {
+  const storageRef = firebase.storage().ref();
+
+  // [START storage_delete_file]
+  // Create a reference to the file to delete
+  var desertRef = storageRef.child('images/desert.jpg');
+
+  // Delete the file
+  desertRef.delete().then(() => {
+    // File deleted successfully
+  }).catch((error) => {
+    // Uh-oh, an error occurred!
+  });
+  // [END storage_delete_file]
+}
+
 /**
  * @param {File} file 
  */
