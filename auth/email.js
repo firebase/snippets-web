b2a0792688a2c4ee67fde5536d2feeab5dfe6470
Frank van Puffelen <frank.vanpuffelen@gmail.com>
2021-01-20 16:00:45 -0800

    Update email.js
    The promises for `signInWithEmailAndPassword` and `createUserWithEmailAndPassword` resolve with a `UserCredential` and not a user. Calling the parameter `user` is a left-over from the old (pre-5.0) SDK where it was an actual `FirebaseUser` object.
    
    I renamed the parameter and added an example of how to access the UID, which is the most common action for developers after creating an account/signing in.

--- auth/email.js
+++ auth/email.js
@@ -9,8 +9,9 @@ function signInWithEmailPassword() {
   var password = "hunter2";
   // [START auth_signin_password]
   firebase.auth().signInWithEmailAndPassword(email, password)
-    .then((user) => {
-      // Signed in 
+    .then((userCredential) => {
+      // Signed in
+      console.log(userCredential.user.uid);
       // ...
     })
     .catch((error) => {
@@ -25,8 +26,9 @@ function signUpWithEmailPasswoerd() {
   var password = "hunter2";
   // [START auth_signup_password]
   firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then((user) => {
+    .then((userCredential) => {
       // Signed in 
+      console.log(userCredential.user.uid);
       // ...
     })
     .catch((error) => {
