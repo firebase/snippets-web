c892949443df2dc1b0d3ae3e1147a54865152c91
Sam <samstern@google.com>
2021-04-09 11:40:14 +0100

    Add RTDB get() snippet

--- database/read-and-write.js
+++ database/read-and-write.js
@@ -123,3 +123,18 @@ function toggleStar(postRef, uid) {
   });
 }
 // [END rtdb_social_star_transaction]
+
+function readOnceWithGet(userId) {
+  // [START rtdb_read_once_get]
+  const dbRef = firebase.database().ref();
+  dbRef.child("users").child(userId).get().then((snapshot) => {
+    if (snapshot.exists()) {
+      console.log(snapshot.val());
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
+  // [END rtdb_read_once_get]
+}
