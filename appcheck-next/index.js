69e54776cb503251ca618837d22533ca53b5e9c1
Kevin Cheung <kevincheung@google.com>
2021-07-28 15:56:18 -0700

    Show isTokenAutoRefreshEnabled in App Check

--- appcheck-next/index.js
+++ appcheck-next/index.js
@@ -13,7 +13,11 @@ function initialize() {
   // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
   // key is the counterpart to the secret key you set in the Firebase console.
   const appCheck = initializeAppCheck(app, {
-    provider: new ReCaptchaV3Provider('abcdefghijklmnopqrstuvwxy-1234567890abcd')
+    provider: new ReCaptchaV3Provider('abcdefghijklmnopqrstuvwxy-1234567890abcd'),
+
+    // Optional argument. If true, the SDK automatically refreshes App Check
+    // tokens as needed.
+    isTokenAutoRefreshEnabled: true
   });
   // [END appcheck_initialize]
 }
@@ -59,7 +63,11 @@ function initializeCustomProvider() {
   });
   
   const appCheck = initializeAppCheck(app, {
-    provider: appCheckCustomProvider
+    provider: appCheckCustomProvider,
+
+    // Optional argument. If true, the SDK automatically refreshes App Check
+    // tokens as needed.
+    isTokenAutoRefreshEnabled: true    
   });
   // [END appcheck_initialize_custom_provider]
 }
