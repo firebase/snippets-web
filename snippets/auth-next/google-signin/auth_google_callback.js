4fda27ade77546e6e032520ff6d5469be3e59248
Kevin Cheung <kevincheung@google.com>
2021-11-19 10:34:42 -0800

    npm run snippets

--- snippets/auth-next/google-signin/auth_google_callback.js
+++ snippets/auth-next/google-signin/auth_google_callback.js
@@ -13,26 +13,21 @@ function onSignIn(googleResponse) {
   // We need to register an Observer on Firebase Auth to make sure auth is initialized.
   const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
     unsubscribe();
-    // Check if we are already signed in to Firebase with the correct user.
+    // Build Firebase credential with the Google ID token.
     const googleIdToken = googleResponse.credential;
-    if (!isUserEqual(googleIdToken, firebaseUser)) {
-      // Build Firebase credential with the Google ID token.
-      const credential = GoogleAuthProvider.credential(googleIdToken);
+    const credential = GoogleAuthProvider.credential(googleIdToken);
 
-      // Sign in with credential from the Google user.
-      signInWithCredential(auth, credential).catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.email;
-        // The credential that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
-      });
-    } else {
-      console.log('User already signed-in Firebase.');
-    }
+    // Sign in with credential from the Google user.
+    signInWithCredential(auth, credential).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The credential that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
   });
 }
 // [END auth_google_callback_modular]
