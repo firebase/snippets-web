e666ba3c181ee53aa4b89f0e4d535ef983cf0662
Emmanuel Mwangi <emmanuelmbuthia10@gmail.com>
2022-03-05 11:08:47 +0300

    added .docs to querySnapshot

--- snippets/firestore-next/test-firestore/get_all_users.js
+++ snippets/firestore-next/test-firestore/get_all_users.js
@@ -8,7 +8,7 @@
 import { collection, getDocs } from "firebase/firestore"; 
 
 const querySnapshot = await getDocs(collection(db, "users"));
-querySnapshot.forEach((doc) => {
+querySnapshot.docs.forEach((doc) => {
   console.log(`${doc.id} => ${doc.data()}`);
 });
 // [END get_all_users_modular]
