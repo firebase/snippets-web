5d29bd415676ee60e64179a022e2bcd3024b7bb7
rosariopf <rosariopf@google.com>
2022-06-10 12:10:42 +0100

    fix syntax error

--- firestore-next/test.firestore.js
+++ firestore-next/test.firestore.js
@@ -1120,23 +1120,23 @@ describe("firestore", () => {
                         name: 'Lincoln Memorial',
                         type: 'memorial'
                     }),
-                    addDoc(collection((citiesRef, 'DC', 'landmarks'), {
+                    addDoc(collection(citiesRef, 'DC', 'landmarks'), {
                         name: 'National Air and Space Museum',
                         type: 'museum'
                     }),
-                    addDoc(collection((citiesRef, 'TOK', 'landmarks'), {
+                    addDoc(collection(citiesRef, 'TOK', 'landmarks'), {
                         name: 'Ueno Park',
                         type: 'park'
                     }),
-                    addDoc(collection((citiesRef, 'TOK', 'landmarks'), {
+                    addDoc(collection(citiesRef, 'TOK', 'landmarks'), {
                         name: 'National Museum of Nature and Science',
                         type: 'museum'
                     }),
-                    addDoc(collection((citiesRef, 'BJ', 'landmarks'), {
+                    addDoc(collection(citiesRef, 'BJ', 'landmarks'), {
                         name: 'Jingshan Park',
                         type: 'park'
                     }),
-                    addDoc(collection((citiesRef, 'BJ', 'landmarks'), {
+                    addDoc(collection(citiesRef, 'BJ', 'landmarks'), {
                         name: 'Beijing Ancient Observatory',
                         type: 'museum'
                     })
