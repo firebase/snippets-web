1fd0e3fbfc0d1bb56120fe1a4b75f85e44225dee
Thammarith <8011543+thammarith@users.noreply.github.com>
2023-02-12 16:54:38 +0700

    Fix incorrect code

--- snippets/auth-next/index/auth_set_language_code.js
+++ snippets/auth-next/index/auth_set_language_code.js
@@ -10,5 +10,5 @@ import { getAuth } from "firebase/auth";
 const auth = getAuth();
 auth.languageCode = 'it';
 // To apply the default browser preference instead of explicitly setting it.
-// firebase.auth().useDeviceLanguage();
+// auth.useDeviceLanguage();
 // [END auth_set_language_code_modular]
