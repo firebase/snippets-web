486e5c67bcb895a00ccab90f1b20cefdba6c9e3a
dwyfrequency <dwyfrequency@users.noreply.github.com>
2023-02-15 13:28:52 -0500

    Update gstatic url for v8 example (#325)

--- messaging/service-worker.js
+++ messaging/service-worker.js
@@ -1,5 +1,5 @@
-import firebase from "firebase/app";
-import "firebase/messaging";
+import firebase from 'firebase/app';
+import 'firebase/messaging';
 
 // See: https://github.com/microsoft/TypeScript/issues/14877
 /** @type {ServiceWorkerGlobalScope} */
@@ -10,8 +10,8 @@ function initInSw() {
   // Give the service worker access to Firebase Messaging.
   // Note that you can only use Firebase Messaging here. Other Firebase libraries
   // are not available in the service worker.
-  importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js');
-  importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging.js');
+  importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+  importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
   // Initialize the Firebase app in the service worker by passing in
   // your app's Firebase config object.
@@ -38,16 +38,18 @@ function onBackgroundMessage() {
 
   // [START messaging_on_background_message]
   messaging.onBackgroundMessage((payload) => {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    console.log(
+      '[firebase-messaging-sw.js] Received background message ',
+      payload
+    );
     // Customize notification here
     const notificationTitle = 'Background Message Title';
     const notificationOptions = {
       body: 'Background Message body.',
       icon: '/firebase-logo.png'
     };
-  
-    self.registration.showNotification(notificationTitle,
-      notificationOptions);
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
   });
   // [END messaging_on_background_message]
 }
