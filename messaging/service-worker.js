5381c34ba14cc7b5073451a6d9809b3ec1e71977
dwyfrequency <jackdwyer@google.com>
2023-02-14 20:15:57 -0500

    Update gstatic url for v8 example

--- messaging/service-worker.js
+++ messaging/service-worker.js
@@ -10,21 +10,23 @@ function initInSw() {
   // Give the service worker access to Firebase Messaging.
   // Note that you can only use Firebase Messaging here. Other Firebase libraries
   // are not available in the service worker.
-  importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js');
-  importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging.js');
+  importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+  importScripts(
+    "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
+  );
 
   // Initialize the Firebase app in the service worker by passing in
   // your app's Firebase config object.
   // https://firebase.google.com/docs/web/setup#config-object
   firebase.initializeApp({
-    apiKey: 'api-key',
-    authDomain: 'project-id.firebaseapp.com',
-    databaseURL: 'https://project-id.firebaseio.com',
-    projectId: 'project-id',
-    storageBucket: 'project-id.appspot.com',
-    messagingSenderId: 'sender-id',
-    appId: 'app-id',
-    measurementId: 'G-measurement-id',
+    apiKey: "api-key",
+    authDomain: "project-id.firebaseapp.com",
+    databaseURL: "https://project-id.firebaseio.com",
+    projectId: "project-id",
+    storageBucket: "project-id.appspot.com",
+    messagingSenderId: "sender-id",
+    appId: "app-id",
+    measurementId: "G-measurement-id",
   });
 
   // Retrieve an instance of Firebase Messaging so that it can handle background
@@ -38,16 +40,18 @@ function onBackgroundMessage() {
 
   // [START messaging_on_background_message]
   messaging.onBackgroundMessage((payload) => {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    console.log(
+      "[firebase-messaging-sw.js] Received background message ",
+      payload
+    );
     // Customize notification here
-    const notificationTitle = 'Background Message Title';
+    const notificationTitle = "Background Message Title";
     const notificationOptions = {
-      body: 'Background Message body.',
-      icon: '/firebase-logo.png'
+      body: "Background Message body.",
+      icon: "/firebase-logo.png",
     };
-  
-    self.registration.showNotification(notificationTitle,
-      notificationOptions);
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
   });
   // [END messaging_on_background_message]
 }
