cdfd8e0f0de51d405db05df23b300440cd4ac6c5
Mark Arndt <markarndt@google.com>
2023-03-23 15:54:07 -0700

    More Database emulator snippet updates.

--- snippets/database-next/emulator-suite/rtdb_emulator_connect.js
+++ snippets/database-next/emulator-suite/rtdb_emulator_connect.js
@@ -10,6 +10,6 @@ import { getDatabase, connectDatabaseEmulator } from "firebase/database";
 const db = getDatabase();
 if (location.hostname === "localhost") {
   // Point to the RTDB emulator running on localhost.
-  connectDatabaseEmulator(db, "localhost", 9000);
+  connectDatabaseEmulator(db, "127.0.0.1", 9000);
 } 
 // [END rtdb_emulator_connect_modular]
