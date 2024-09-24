23ec90ae77ad0025d779e0b8a86a0740df481275
Daniel La Rocque <dlarocque@google.com>
2024-09-24 11:02:24 -0400

    Upgrade versions in FCM service worker

--- messaging/service-worker.js
+++ messaging/service-worker.js
@@ -10,8 +10,8 @@ function initInSw() {
   // Give the service worker access to Firebase Messaging.
   // Note that you can only use Firebase Messaging here. Other Firebase libraries
   // are not available in the service worker.
-  importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
-  importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+  importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js');
+  importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
 
   // Initialize the Firebase app in the service worker by passing in
   // your app's Firebase config object.
