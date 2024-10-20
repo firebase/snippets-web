c671730fd660d25d33a02ae47b554ea5f5f20103
rinarakaki <rnarkkx@gmail.com>
2024-10-20 09:36:57 +0000

    Update auth-next/custom.js

--- snippets/auth-next/custom/auth_sign_in_custom.js
+++ snippets/auth-next/custom/auth_sign_in_custom.js
@@ -8,15 +8,14 @@
 import { getAuth, signInWithCustomToken } from "firebase/auth";
 
 const auth = getAuth();
-signInWithCustomToken(auth, token)
-  .then((userCredential) => {
-    // Signed in
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ...
-  });
+try {
+  const userCredential = await signInWithCustomToken(auth, token);
+  // Signed in
+  const user = userCredential.user;
+  // ...
+} catch (error) {
+  const errorCode = error.code;
+  const errorMessage = error.message;
+  // ...
+}
 // [END auth_sign_in_custom_modular]
