16e7b7484bcc89018875991a2b87dbedcd2871c9
rinarakaki <rnarkkx@gmail.com>
2024-10-20 09:27:08 +0000

    Update auth-next/email-link-auth.js

--- snippets/auth-next/email-link-auth/auth_email_link_send.js
+++ snippets/auth-next/email-link-auth/auth_email_link_send.js
@@ -8,17 +8,16 @@
 import { getAuth, sendSignInLinkToEmail } from "firebase/auth";
 
 const auth = getAuth();
-sendSignInLinkToEmail(auth, email, actionCodeSettings)
-  .then(() => {
-    // The link was successfully sent. Inform the user.
-    // Save the email locally so you don't need to ask the user for it again
-    // if they open the link on the same device.
-    window.localStorage.setItem('emailForSignIn', email);
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ...
-  });
+try {
+  await sendSignInLinkToEmail(auth, email, actionCodeSettings);
+  // The link was successfully sent. Inform the user.
+  // Save the email locally so you don't need to ask the user for it again
+  // if they open the link on the same device.
+  window.localStorage.setItem('emailForSignIn', email);
+  // ...
+} catch (error) {
+  const errorCode = error.code;
+  const errorMessage = error.message;
+  // ...
+}
 // [END auth_email_link_send_modular]
