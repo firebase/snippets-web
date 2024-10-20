400528d6f0fefde057e9e5ff04fd2a68c8845362
rinarakaki <rnarkkx@gmail.com>
2024-10-20 09:35:59 +0000

    Update auth-next/auth-state-persistence.js

--- snippets/auth-next/auth-state-persistence/auth_set_persistence_none.js
+++ snippets/auth-next/auth-state-persistence/auth_set_persistence_none.js
@@ -8,17 +8,16 @@
 import { getAuth, setPersistence, signInWithRedirect, inMemoryPersistence, GoogleAuthProvider } from "firebase/auth";
 
 const auth = getAuth();
-setPersistence(auth, inMemoryPersistence)
-  .then(() => {
-    const provider = new GoogleAuthProvider();
-    // In memory persistence will be applied to the signed in Google user
-    // even though the persistence was set to 'none' and a page redirect
-    // occurred.
-    return signInWithRedirect(auth, provider);
-  })
-  .catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
+try {
+  await setPersistence(auth, inMemoryPersistence);
+  const provider = new GoogleAuthProvider();
+  // In memory persistence will be applied to the signed in Google user
+  // even though the persistence was set to 'none' and a page redirect
+  // occurred.
+  return signInWithRedirect(auth, provider);
+} catch (error) {
+  // Handle Errors here.
+  const errorCode = error.code;
+  const errorMessage = error.message;
+}
 // [END auth_set_persistence_none_modular]
