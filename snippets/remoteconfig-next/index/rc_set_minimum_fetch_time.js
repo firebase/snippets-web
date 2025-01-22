4ae62fc3f30fdb2991ac0aa18b15897e74ff2b61
Athira M <athiramanu@google.com>
2025-01-22 19:10:45 +0530

    Add comment to highlight the default prod value for minimumFetchIntervalMillis.
    Issue addressed: https://github.com/firebase/firebase-js-sdk/issues/2841

--- snippets/remoteconfig-next/index/rc_set_minimum_fetch_time.js
+++ snippets/remoteconfig-next/index/rc_set_minimum_fetch_time.js
@@ -5,5 +5,6 @@
 // 'npm run snippets'.
 
 // [START rc_set_minimum_fetch_time_modular]
+// The default and recommended production fetch interval for Remote Config is 12 hours.
 remoteConfig.settings.minimumFetchIntervalMillis = 3600000;
 // [END rc_set_minimum_fetch_time_modular]
